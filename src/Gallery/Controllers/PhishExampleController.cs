9b2c5829aee58adab53fe82379a6b427fb2d5148
Jason E Smith <smith1jason@users.noreply.github.com>
2018-06-22 12:52:05 -0400

    Added Example Detail cshtml

--- src/Gallery/Controllers/PhishExampleController.cs
+++ src/Gallery/Controllers/PhishExampleController.cs
@@ -10,12 +10,7 @@ namespace Gallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from Lake O'Phishy!");
-                       
+            return View();                       
         }
     }
 }
